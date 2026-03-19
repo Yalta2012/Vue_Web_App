7fad27c795e953074040870632b2c5be00dbba51
egor <egor>
2026-03-20 00:21:34 +0500

    PrimeVue

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,34 @@
 <script setup>
 import { ref, computed, onMounted } from 'vue';
 import { useAuthStore } from '@/stores/authStore';
+import Button from 'primevue/button';
+import Menubar from 'primevue/menubar';
+import InputText from 'primevue/inputtext';
 
 
 const email = ref('');
 const password = ref('')
 const authStore = useAuthStore()
 
+const items = ref([
+    {
+        label: 'Главная страница',
+        icon: 'pi pi-fw pi-home',
+        route: '/',
+    },
+
+    {
+        label: 'Профиль',
+        icon: 'pi pi-fw pi-user',
+        route: 'profile',
+    },
+
+    {
+        label: 'Список объектов',
+        icon: 'pi pi-fw pi-box',
+        route: 'property',
+    }
+]);
 const isAuthenticated = computed(() => { return authStore.isAuthenticated })
 const user = computed(() => { return authStore.user })
 const authError = computed(() => { return authStore.errorMessage })
@@ -34,37 +56,45 @@ onMounted(async () => {
 </script>
 
 <template>
-    <header>
-        <nav>
-            <ul>
-                <li><router-link to="/">Главная</router-link></li>
-                <li><router-link to="/profile">Профиль</router-link></li>
-                <li><router-link to="/property">Список объектов</router-link></li>
-            </ul>
+    <Menubar :model="items">
 
-            <div v-if="isAuthenticated && user">
-                Welcome, {{ user.first_name }}
-                <button @click="logout">Logout</button>
+        <template #start>
+            <span>
+                <img src="@/assets/logo.svg" width="50" alt="My SVG Icon">
+            </span>
+        </template>
+
+        <template #item="{ item, props, hasSubmenu, root }">
+            <a class="flex items-cenetr ml-6 p-4">
+                <router-link v-if="item.route" :to="item.route">
+                    <span :class="item.icon" />
+                    <span class="ml-1">{{ item.label }}</span>
+                </router-link>
+            </a>
+        </template>
+
+        <template #end>
+            <div class="flex items-center gap-2">
+                <div v-if="isAuthenticated && user">
+                    <span class="pi pi-fw pi-user mr-4" /> {{ user.first_name }}
+                    <Button @click="logout" class="ml-4">Logout</Button>
+                </div>
+
+                <div v-else>
+                    <form @submit.prevent="login">
+                        <InputText v-model="email" type="email" id="email" required placeholder="Login"
+                            class="m-2 sm:w-auto" :class="{ 'p-invalid': authError }" />
+                        <InputText v-model="password" type="password" id="password" required placeholder="Password"
+                            class="m-2 sm:w-auto" :class="{ 'p-invalid': authError }" />
+                        <Button type="submit">Enter</Button>
+                        <div class="ml-2"><small v-if="authError" class="error">{{ authError }}</small></div>
+                    </form>
+                </div>
             </div>
 
-            <div v-else>
-                <form @submit.prevent="login">
-                    <div>
-                        <label for="email">Email:</label>
-                        <input v-model="email" type="email" id="email" required>
-                    </div>
 
-                    <div>
-                        <label for="password">Password:</label>
-                        <input v-model="password" type="password" id="password" required>
-                    </div>
-
-                    <button type="submit">Login</button>
-                    <p v-if="authError" class="error">{{ authError }}</p>
-                </form>
-            </div>
-        </nav>
-    </header>
+        </template>
+    </Menubar>
 
 </template>
 
@@ -75,7 +105,7 @@ onMounted(async () => {
 
 header {
     border: 1px solid;
-    padding: 10px;
+    /* padding: 10px; */
     border-radius: 10px;
     font-size: small;
 }
